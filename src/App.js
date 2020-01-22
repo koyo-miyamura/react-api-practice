4ae41fff555f9b9fd980e9587e575cfd918e6680
koyo-miyamura <nexus9071@gmail.com>
2020-01-23 01:34:10 +0900

    APIリクエストしてみる

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios('https://api.github.com/repos/koyo-miyamura/vue_api_spa/pulls',{
+        params: {
+          state: 'closed'
+        }
+      })
+      setData(results.data)
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div>Hello React!!</div>
+    <div>
+      <h1>Hello React!!</h1>
+      <ul>
+        {data.map((pr) => (
+          <li key={pr.id}>{pr.title}<a href={pr.html_url}>{pr.html_url}</a></li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
